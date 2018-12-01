97c01e1ee349d1f0f899c04a7ce36e7be3dd70c2
Marcelo Ribeiro de Albuquerque <re035153@resource.com.br>
2018-12-01 16:11:52 -0200

    Inclusão testes de inserção de colaborador (repositório)

---
+++ test/OpenId3as.DivulgacaoONGs.Infra.Data.Test/CollaboratorTests.cs
@@ -0,0 +1,49 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using OpenId3as.DivulgacaoONGs.Domain.Entities.Collaborators;
+using OpenId3as.DivulgacaoONGs.Domain.Interfaces.Repositories;
+using OpenId3as.DivulgacaoONGs.Infra.Data.Context.Postgres;
+using System;
+
+namespace OpenId3as.DivulgacaoONGs.Infra.Data.Test
+{
+    [TestFixture]
+    public class CollaboratorTests
+    {
+        private NOGContext context;
+        private ICollaboratorRepository collaboratorRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            context = Substitute.For<NOGContext>();
+            collaboratorRepository = Substitute.For<ICollaboratorRepository>();
+        }
+
+        [Test]
+        public void Collaborators_InsertCollaborator_InsertedSuccessfully()
+        {
+            var newCollaborator = new Collaborator()
+            {
+                FirstName = "first",
+                LastName = "last",
+                Email = "test@test.com",
+                Birth = DateTime.Now
+            };
+
+            var insertedCollaborator = new Collaborator()
+            {
+                Id = 1,
+                FirstName = "first",
+                LastName = "last",
+                Email = "test@test.com",
+                Birth = DateTime.Now
+            };
+
+            var result = collaboratorRepository.Add(newCollaborator).Returns(insertedCollaborator);
+
+            Assert.NotNull(result, message: "The result is null");
+            Assert.AreNotEqual(insertedCollaborator.Id, newCollaborator.Id, message: "The id is equal");
+        }
+    }
+}
